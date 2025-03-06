faee0c6d688d0f6f359862653bf9629a32544852
junhyeok020630 <skylove2424@gmail.com>
2025-03-06 11:57:59 +0900

    Initial commit

---
+++ ex29_jpa_board_rest_api/src/main/java/com/example/ex29_jpa_board_rest_api/MyController.java
@@ -0,0 +1,93 @@
+package com.example.ex29_jpa_board_rest_api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ex29_jpa_board_rest_api.entity.Board;
+import com.example.ex29_jpa_board_rest_api.service.BoardService;
+
+@RestController
+@RequestMapping("/api/board")
+public class MyController {
+    
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping
+    public String root() throws Exception {
+        return "JPA Board API!";
+    }
+
+    @RequestMapping(value = "/user-list", method = RequestMethod.GET)
+    public List<Board> userlistPage() {
+        return boardService.list();
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Board> view(@RequestParam("id") int id) {
+        Board board = boardService.view(id);
+        if(board != null) {
+            return ResponseEntity.ok(board);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/write")
+    public ResponseEntity<Board> write(@RequestBody Board board) {
+        boardService.write(board.getWriter(), board.getTitle(), board.getContent());
+
+        return ResponseEntity.ok(board);
+    }
+    
+    // @PostMapping("/write")
+    // public ResponseEntity<Board> write(@RequestParam("writer") String writer, 
+    //         @RequestParam("title") String title, 
+    //         @RequestParam("content") String content) {
+    //     boardService.write(writer, title, content);
+
+    //     Board newBoard = new Board();
+    //     newBoard.setWriter(writer);
+    //     newBoard.setTitle(title);
+    //     newBoard.setContent(content);
+
+    //     return ResponseEntity.ok(newBoard);
+    // }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        boardService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    // @DeleteMapping("/delete")
+    // public ResponseEntity<Void> delete(@RequestParam("id") int id) {
+    //     boardService.delete(id);
+
+    //     return ResponseEntity.noContent().build();
+    // }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Board> update(@PathVariable int id, @RequestBody Board updatedBoard) {
+        try{
+            Board board = boardService.update(id, updatedBoard.getWriter(), updatedBoard.getTitle(), updatedBoard.getContent());
+            return ResponseEntity.ok(board);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+}
---
+++ ex29_jpa_board_rest_api/src/main/java/com/example/ex29_jpa_board_rest_api/service/BoardService.java
@@ -0,0 +1,52 @@
+package com.example.ex29_jpa_board_rest_api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.ex29_jpa_board_rest_api.entity.Board;
+import com.example.ex29_jpa_board_rest_api.repository.BoardRepository;
+
+@Service
+public class BoardService {
+    
+    @Autowired
+    private BoardRepository boardRepository;
+
+    public List<Board> list() {
+        return  boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    public Board view(int id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    // public void write(String writer, String title, String content) {
+    //     Board board = new Board();
+        
+    //     board.setWriter(writer);
+    //     board.setTitle(title);
+    //     board.setContent(content);
+
+    //     boardRepository.save(board);
+    // }
+    
+    public void write(String writer, String title, String content) {
+        boardRepository.write(writer, title, content);
+    }
+
+    public void delete(int id) {
+        boardRepository.deleteById(id);
+    }
+
+    public Board update(int id, String writer, String title, String content) {
+        Board board = boardRepository.findById(id).orElseThrow(()-> new RuntimeException("게시글을 찾을 수 없습니다. ID: " + id));
+        board.setWriter(writer);
+        board.setTitle(title);
+        board.setContent(content);
+
+        return boardRepository.save(board);
+    }
+}
